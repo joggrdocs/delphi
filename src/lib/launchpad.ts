f366ed28b8b97257a2767fca1c792ff505eafc78
Zac Rosenbauer <zacrosenbauer@gmail.com>
2021-08-02 17:47:26 -0500

    Add initial deployment process

--- src/lib/launchpad.ts
+++ src/lib/launchpad.ts
@@ -1,23 +1,80 @@
-// import axios from 'axios';
-//
-// interface Config {
-//   url: string;
-//   branch?: string;
-//   commit?: string;
-// }
-//
-// interface Service {
-//   apiKey: string;
-//   name: string;
-// }
-//
-// export async function deploy (config: Config, service: Service): Promise<void> {
-//   const result = await axios.post(`${config.url}/deployments`, {
-//     apiKey: service.apiKey,
-//     name: service.name,
-//     branch: config.branch,
-//     commit: config.commit
-//   });
-//
-//   // Poll for updates aka is released?
-// }
+import * as github from '@actions/github';
+import axios from 'axios';
+
+import { getBranch } from './github';
+
+const API_URL = 'alpha-launchpad.bluenova-app.com';
+
+// Types
+// -----
+
+export interface Deployment {
+  url: string;
+}
+
+export interface Organization {
+  projectId: string;
+  slugId: string;
+}
+
+// LaunchPad Class
+// -----
+
+interface LaunchPadConfig {
+  apiKey: string;
+  name: string;
+}
+
+export default class LaunchPad {
+  public slugId?: string;
+  public projectId?: string;
+  private readonly apiKey: string;
+  private readonly name: string;
+  private readonly repository: string;
+  private readonly branch: string;
+  private readonly commit: string;
+  private isSetup = false;
+
+  constructor (props: LaunchPadConfig) {
+    this.apiKey = props.apiKey;
+    this.name = props.name;
+    this.commit = github.context.sha;
+    this.repository = github.context.repo.repo;
+    this.branch = getBranch();
+  }
+
+  public async setup (): Promise<void> {
+    const organization = await this.readOrganization();
+
+    this.projectId = organization.projectId;
+    this.slugId = organization.slugId;
+
+    this.isSetup = true;
+  }
+
+  public async createDeployment (): Promise<Deployment> {
+    this.assertSetup();
+
+    const result = await axios.post(`${API_URL}/deployments`, {
+      apiKey: this.apiKey,
+      name: this.name,
+      branch: this.branch,
+      repository: this.repository,
+      commit: this.commit
+    });
+
+    return result.data;
+  }
+
+  private async readOrganization (): Promise<Organization> {
+    const result = await axios.get(`${API_URL}/organizations/${this.apiKey}`);
+
+    return result.data;
+  }
+
+  private assertSetup () {
+    if (!this.isSetup) {
+      throw new Error('You must call the "LaunchPad#setup" method before running any commands');
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,51 @@
 import * as core from '@actions/core';
 
+import * as content from './lib/content';
+import LaunchPad from './lib/launchpad';
+import * as github from './lib/github';
+import Docker from './lib/docker';
+
 async function run (): Promise<void> {
   try {
-    // Inputs: Docker file location
+    const serviceAccountKey = core.getInput('service_account_key');
+    const apiKey = core.getInput('api_key');
+    const name = core.getInput('name');
 
-    // RUN: Build Image
-    // RUN: Push Image (VERY CAREFUL)
-    // RUN: Deploy Service
-    // RUN: Edit Description (similar to dependabot)
-    // RUN: Inject Preview into description
+    // Update description that a deploy is in flight
+    if (github.isPullRequest()) {
+      await github.prependToPullDescription(
+        content.getRunningDescription()
+      );
+    }
 
+    const launchpad = new LaunchPad({
+      name,
+      apiKey
+    });
+    await launchpad.setup();
+
+    // Build & Push Image to LaunchPad repository
+    const docker = new Docker({
+      serviceAccountKey,
+      name,
+      projectId: launchpad.projectId as string,
+      slug: launchpad.slugId as string,
+      apiKey: apiKey
+    });
+    await docker.setup();
+    await docker.buildAndPush();
+
+    // Deploy built image to LaunchPad Cloud
+    const result = await launchpad.createDeployment();
+
+    // Add Preview URL to PR
+    if (github.isPullRequest()) {
+      await github.appendToPullDescription(
+        content.getFinishedDescription(result.url)
+      );
+    }
   } catch (error) {
+    await github.resetPullDescription();
     core.setFailed(error.message);
   }
 }
