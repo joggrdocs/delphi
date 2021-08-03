96e3d4e157e2c43b963ff8438551a5636cb0e324
Zac Rosenbauer <zacrosenbauer@gmail.com>
2021-08-02 22:11:41 -0500

    test a real world

--- examples/nodejs-simple/server.js
+++ examples/nodejs-simple/server.js
@@ -3,7 +3,14 @@ const express = require('express');
 const server = express();
 
 server.get('/hello', (req, res) => {
-  res.json({message: 'Hello World!'});
+  res.json({
+    date: (new Date()).toISOString(),
+    message: 'Hello World!'
+  });
+});
+
+server.use('*', (req, res) => {
+  res.json({ message: 'Not Found' });
 });
 
 server.listen(8080, (err) => {
