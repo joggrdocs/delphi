32509949b87fba2d8bf37484cc61915fc447ee96
Zac Rosenbauer <zac@bluenova.io>
2021-08-10 19:15:51 -0500

    Update server.js

--- examples/nodejs-simple/server.js
+++ examples/nodejs-simple/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const server = express();
 
 server.get('/hello', (req, res) => {
-  res.json({message: 'Hello World!'});
+  res.json({ message: 'Hello World!!!' });
 });
 
 server.listen(8080, (err) => {
