33fc087f3deaf3095f0ba31c1da0b242a25b6179
Zac Rosenbauer <zacrosenbauer@gmail.com>
2022-04-17 20:22:37 -0500

    better example

--- examples/nodejs-simple/server.js
+++ examples/nodejs-simple/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const server = express();
 
+server.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+
 server.get('/hello', (req, res) => {
   res.json({
     message: process.env.MESSAGE || 'Hello World!'
@@ -11,5 +15,5 @@ server.get('/hello', (req, res) => {
 server.listen(3000, (err) => {
   if (err) throw err;
 
-  console.log('Server started on port 8080');
+  console.log('Server started on port 3000');
 });
