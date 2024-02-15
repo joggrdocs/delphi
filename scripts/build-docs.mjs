572056326d30ebd7de41e657264097d6b7dca480
Zac Rosenbauer <zacrosenbauer@gmail.com>
2024-02-15 16:00:13 -0500

    💽 incremental change

--- scripts/build-docs.mjs
+++ scripts/build-docs.mjs
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 const run = () => {
   process.stdout.write('📚 Building docs...\n');
 
-  const readmePath = path.resolve(__dirname, path.join('..', '..', 'README.md'));
+  const readmePath = path.resolve(__dirname, path.join('..', 'README.md'));
 
   process.stdout.write('📚 Generating README.md...\n');
   process.stdout.write(readmePath);
