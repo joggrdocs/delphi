9a8466bbfc96472c17e3c51fe67950baf68af7f7
Zac Rosenbauer <zacrosenbauer@gmail.com>
2024-02-15 16:09:49 -0500

    💽 incremental change

--- scripts/build-docs.mjs
+++ scripts/build-docs.mjs
@@ -4,16 +4,59 @@ import path from 'node:path';
 import fs from 'node:fs';
 import { fileURLToPath } from 'node:url';
 import tempo from '@joggr/tempo';
+import YAML from 'yaml';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const FILEPATH_README = path.resolve(__dirname, path.join('..', 'README.md'));
+const FILEPATH_ACTION_CONFIG = path.resolve(__dirname, path.join('..', 'action.yaml'));
+
+const COMMENT_START = '<!-- docs:start -->';
+const COMMENT_END = '<!-- docs:end -->';
+
 const run = () => {
   process.stdout.write('📚 Building docs...\n');
+  try {
+    const actionYaml = fs.readFileSync(FILEPATH_ACTION_CONFIG, 'utf8');
+    const parsedActionYaml = YAML.parse(actionYaml);
+    
+    const inputs = [];
+    const outputs = [];
 
-  const readmePath = path.resolve(__dirname, path.join('..', 'README.md'));
+    for (const [key, value] of Object.entries(parsedActionYaml.inputs)) {
+      inputs.push([key, value.required ? 'yes' : 'no', value.description ?? 'no description provided', value.default ?? '-']);
+    }
 
-  process.stdout.write('📚 Generating README.md...\n');
-  process.stdout.write(readmePath);
+    for (const [key, value] of Object.entries(parsedActionYaml.outputs)) {
+      outputs.push([key, value.description ?? 'no description provided']);
+    }
+
+    const api = tempo.default()
+      .h3('Inputs')
+      .table([
+        ['Field', 'Required', 'Description', 'Default'],
+        ...inputs,
+      ])
+      .h3('Outputs')
+      .table([
+        ['Field', 'Description'],
+        ...outputs,
+      ])
+      .toString();
+
+    const readme = fs.readFileSync(FILEPATH_README, 'utf8');
+    const splitReadme = readme.split(COMMENT_START);
+    const splitReadmeEnd = splitReadme[1].split(COMMENT_END);
+    const newReadme = `${splitReadme[0]}${COMMENT_START}\n${api}\n${COMMENT_END}${splitReadmeEnd[1]}`;
+
+    // Write the new README
+    fs.writeFileSync(FILEPATH_README, newReadme);
+
+    process.stdout.write('✅ Completed Building Docs\n');
+  } catch (error) {
+    process.stderr.write(`❌ Error: ${error.message}\n`);
+    process.exit(1);
+  }
 };
 run();
